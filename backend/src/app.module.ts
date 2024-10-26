6e82de52a8ab9d6c67b56954d8b9c0d19d83e2da
ikabeee <carlglz30@gmail.com>
2024-10-26 16:48:11 -0500

    Imports modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
 import { UniversitiesModule } from './universities/universities.module';
+import { ScolarshipsModule } from './scolarships/scolarships.module';
+import { MunicipalitiesModule } from './municipalities/municipalities.module';
+import { AdminsModule } from './admins/admins.module';
+import { UsersModule } from './users/users.module';
+import { AnnouncementsModule } from './announcements/announcements.module';
+import { StudentsServicesModule } from './students-services/students-services.module';
+import { InfrastructureModule } from './infrastructure/infrastructure.module';
+import { JobOpportunitiesModule } from './job-opportunities/job-opportunities.module';
+import { CareersModule } from './careers/careers.module';
+import { SubjectsModule } from './subjects/subjects.module';
+import { ExtracurricularActivitiesModule } from './extracurricular-activities/extracurricular-activities.module';
+import { LanguageCoursesModule } from './language-courses/language-courses.module';
+import { AgreementsModule } from './agreements/agreements.module';
+import { EventsListModule } from './events_list/events_list.module';
+
+
 
 @Module({
-  imports: [UniversitiesModule],
+  imports: [UniversitiesModule, ScolarshipsModule, MunicipalitiesModule, AdminsModule, UsersModule, AnnouncementsModule, StudentsServicesModule, InfrastructureModule, JobOpportunitiesModule, CareersModule, SubjectsModule, ExtracurricularActivitiesModule, LanguageCoursesModule, AgreementsModule, EventsListModule],
 })
 export class AppModule {}
