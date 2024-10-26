8e34a8e990e33f80eaa01ecbb9c0ec7dbd8bc0e2
ikabeee <carlglz30@gmail.com>
2024-10-26 16:18:51 -0500

    Add university module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
+import { UniversitiesModule } from './universities/universities.module';
 
 @Module({
-  imports: [],
+  imports: [UniversitiesModule],
 })
 export class AppModule {}
