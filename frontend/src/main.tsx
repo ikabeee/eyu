340f1920954da9557d4e68bc27818db3250e2c68
ikabeee <carlglz30@gmail.com>
2024-10-26 20:03:29 -0500

    Add react router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,13 +4,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <NextUIProvider>
+    <BrowserRouter>
+      <NextUIProvider>
         <PrimeReactProvider>
           <App />
         </PrimeReactProvider>
-    </NextUIProvider>
+      </NextUIProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
